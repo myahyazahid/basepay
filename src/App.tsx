0f4cf8e7bf6d6aa5953e66f4ff7380218f0fdc2e
Redscale <myahyazahid11@gmail.com>
2026-01-21 17:46:08 +0700

    add vercell speed insight

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Dashboard from './pages/Dashboard';
-import Profile from './pages/Profile';
-import Scan from './pages/Scan';
-import Request from './pages/Request';
-import History from './pages/History';
-import AnalyticsPage from './pages/Analytics';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
+import Scan from "./pages/Scan";
+import Request from "./pages/Request";
+import History from "./pages/History";
+import AnalyticsPage from "./pages/Analytics";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
-import Rewards from './pages/Rewards';
-import { Analytics } from '@vercel/analytics/react'
+import Rewards from "./pages/Rewards";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
@@ -23,7 +24,8 @@ function App() {
         <Route path="/analytics" element={<AnalyticsPage />} />
         <Route path="/rewards" element={<Rewards />} />
       </Routes>
-        <Analytics />
+      <Analytics />
+      <SpeedInsights />
     </BrowserRouter>
   );
 }
